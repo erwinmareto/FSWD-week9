4830fb331bfe3c9aa3567036714592802d0022f9
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2023-08-05 23:55:18 +0700

    add logging

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const swaggerUI = require("swagger-ui-express");
 const errorHandler = require('./middleware/errHandler.js');
 const swaggerJson = require('./swagger.json')
 const cors = require('cors');
+const morgan = require('morgan')
 
 const app = express();
 
+// Logging
+app.use(morgan('tiny'));
 // API docs
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJson));
 // Sharing recourses
